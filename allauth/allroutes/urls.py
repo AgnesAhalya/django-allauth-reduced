25bda8e59ae4a778edac7bdf3a9bd96efe6051a7
Agnes Ahalya <agnesarogyaraj@gmail.com>
2026-07-01 11:12:59 +0200

    Adding urls

--- allauth/allroutes/urls.py
+++ allauth/allroutes/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -9,4 +9,13 @@ urlpatterns = [
     path("okta/", views.okta, name="okta"),
     path("netiq/", views.netiq, name="netiq"),
     path("idp/", views.idp, name="idp"),
+
+    # MFA recovery codes
+    path("accounts/2fa/recovery-codes/", include("allauth.mfa.recovery_codes.urls")),
+
+    # Instagram provider
+    path("accounts/", include("allauth.socialaccount.providers.instagram.urls")),
+
+    # IDP/OIDC provider
+    path("idp/oidc/", include("allauth.idp.urls")),
 ]
